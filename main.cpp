619a2ce38a64e7d1b63ed0786f1359671f024bf7
HarutoTanaka-go <k024g0048@g.neec.ac.jp>
2026-01-15 01:36:41 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int Recursive(int kyuuryou, int zikan, int kotei)
+{
+    if (kyuuryou >= kotei)
+    {
+        printf("%dŠÔŒã‚É’´‚¦‚é", zikan);
+        return zikan;
+    }
+
+
+
+    printf("%d@%d\n", kyuuryou, kotei);
+    kyuuryou = kyuuryou * 2 - 50;
+
+    return (Recursive(kyuuryou, ++zikan, kotei += 1072));
+}
+
+int main()
+{
+
+    int saiki = 100;
+    int ippan = 1226;
+    int zikan = 0;
+    int resullt1 = Recursive(saiki, zikan, ippan);
+
+    return 0;
+}
